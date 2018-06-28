4c18ecc29a5434d20da0b7879cb166e930cc5381
wudiku <344259054@qq.com>
2018-06-28 23:15:08 +0800

    up

--- src/main/java/visual/UiServer.java
+++ src/main/java/visual/UiServer.java
@@ -10,20 +10,61 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import data.TestDataSet;
 import fi.iki.elonen.NanoHTTPD;
+import io.grpc.stub.StreamObserver;
 import lombok.Data;
+import net.PServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Data
-public class UiServer extends NanoHTTPD {
+public class UiServer extends NanoHTTPD implements UiServerGrpc.UiServer {
+
+	static Logger logger = LoggerFactory.getLogger(UiServer.class);
 
 	ObjectMapper objectMapper = new ObjectMapper();
+
+	ConcurrentHashMap<String, List<Float>> xs = new ConcurrentHashMap<>();
+
+	ConcurrentHashMap<String, List<Float>> ys = new ConcurrentHashMap<>();
+
 
 	public UiServer() throws IOException {
 		super(8888);
 		start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
 		System.out.println("\nRunning! Point your browsers to http://localhost:8888/ \n");
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				AtomicInteger l = new AtomicInteger(0);
+				while (true) {
+					synchronized (this) {
+						List<Float> x = xs.putIfAbsent("loss", Lists.newArrayList());
+						if (x == null) {
+							x = xs.putIfAbsent("loss", Lists.newArrayList());
+						}
+						List<Float> y = ys.putIfAbsent("loss", Lists.newArrayList());
+						if (y == null) {
+							y = ys.putIfAbsent("loss", Lists.newArrayList());
+						}
+						x.add((float) l.getAndIncrement());
+						y.add(ThreadLocalRandom.current().nextFloat());
+					}
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
 	}
 
 	public static void main(String args[]) {
@@ -34,14 +75,24 @@ public class UiServer extends NanoHTTPD {
 		}
 	}
 
-	public Response serve(IHTTPSession session) {
+	public synchronized Response serve(IHTTPSession session) {
 		Map<String, String> params = session.getParms();
-		if ("loss".equals(params.get("act"))) {
-			Map<String, Object> maps = Maps.newConcurrentMap();
-			maps.put("x", Lists.newArrayList(1, 2, 3));
-			maps.put("y", Lists.newArrayList(Math.random(), Math.random(), Math.random()));
+		if ("data".equals(params.get("act"))) {
+			int step = Integer.parseInt(params.get("step"));
+			Map<String, Object> result = Maps.newHashMap();
+			for (String key : xs.keySet()) {
+				List<Float> x = xs.get(key);
+				List<Float> y = ys.get(key);
+				if (x == null || y == null || x.size() < step) {
+					continue;
+				}
+				Map<String, Object> tmp = Maps.newHashMap();
+				tmp.put("x", x.subList(step, x.size()));
+				tmp.put("y", y.subList(step, y.size()));
+				result.put(key, tmp);
+			}
 			try {
-				return newFixedLengthResponse(objectMapper.writeValueAsString(maps));
+				return newFixedLengthResponse(objectMapper.writeValueAsString(result));
 			} catch (JsonProcessingException e) {
 				return newFixedLengthResponse(e.getMessage());
 			}
@@ -50,4 +101,18 @@ public class UiServer extends NanoHTTPD {
 		}
 	}
 
+	@Override
+	public synchronized void plot(PlotMessage request, StreamObserver<PlotMessage> responseObserver) {
+		String key = request.getId();
+		List<Float> y = ys.putIfAbsent(key, Lists.newArrayList());
+		List<Float> x = xs.putIfAbsent(key, Lists.newArrayList());
+		assert y != null;
+		synchronized (y) {
+			y.addAll(request.getData().getYList());
+		}
+		assert x != null;
+		synchronized (x) {
+			x.addAll(request.getData().getXList());
+		}
+	}
 }
