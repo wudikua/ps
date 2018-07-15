c70f9a0f12e0b2c43ba7e3a60fed5567dc9da491
meng.jun <meng.jun@immomo.com>
2018-07-15 17:59:41 +0800

    up

--- src/main/java/layer/Layer.java
+++ src/main/java/layer/Layer.java
@@ -68,4 +68,11 @@ public abstract class Layer {
 	public boolean isInput() {
 		return isInput;
 	}
+
+	@Override
+	public String toString() {
+		return "Layer{" +
+				"name='" + name + '\'' +
+				'}';
+	}
 }
